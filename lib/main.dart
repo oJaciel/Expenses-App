fbef956b6ac1079ab0a6170f381756db88183df5
oJaciel <jacielhbarichellO@gmail.com>
2024-11-02 14:59:12 -0300

    Adicionando Imagem
    Registrada imagem no pubspec, e adicionada a imagem na lista de transações, para ser mostrada quando não há nenhum registro.

--- lib/main.dart
+++ lib/main.dart
@@ -49,8 +49,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
-    Transaction(
+  final List<Transaction> _transactions = [
+    /* Transaction(
       id: 't1',
       title: 'Novo Tênis',
       value: 310.76,
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       title: 'Conta de luz',
       value: 211.30,
       date: DateTime.now(),
-    )
+    ) */
   ];
 
   _addTransaction(String title, double value) {
